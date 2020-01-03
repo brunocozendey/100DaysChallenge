5d3afbcbcfb5290ff23c32cd4408478f8c0da42a
Bruno Cozendey <brunocozendey@users.noreply.github.com>
2020-01-02 22:19:09 -0300

    Update tkinter-teste.py

--- python/tkinter-teste.py
+++ python/tkinter-teste.py
@@ -4,4 +4,6 @@ from Tkinter import *   #importa a bibioteca do TKinter
 root = Tk()             #cria a aplicação raiz. 
 w = Label(root, text="Teste TKinter!")    #cria um label com o texto especificado
 w.pack()                                      #insere na tela
+#from Tkinter import ttk
+#ttk.Button(root, text="Hello World").grid() #Cria botão
 root.mainloop()                               #inicializa o loop
